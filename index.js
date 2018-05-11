316f84532e2274d2ef194744b706baef2f37700b
Matt Kane <ascorbic@gmail.com>
2018-05-11 13:52:44 +0100

    Intial commit

---
+++ index.js
@@ -0,0 +1,34 @@
+module.exports = {
+    extends: [
+        "es/2015/client",
+        "plugin:react/recommended",
+        "plugin:import/recommended",
+        "plugin:prettier/recommended",
+        "prettier/react"
+    ],
+    plugins: ["jest", "prettier"],
+    parserOptions: {
+        sourceType: "module",
+        ecmaVersion: 2018,
+        ecmaFeatures: {
+            impliedStrict: true
+        }
+    },
+    env: {
+        browser: true,
+        "jest/globals": true
+    },
+    rules: {
+        "babel/object-curly-spacing": "always",
+        "react/jsx-indent": false,
+        "sort-imports": 0,
+        "react/jsx-filename-extension": [1, { extensions: [".js", ".jsx"] }],
+        "no-console": 0,
+        "import/no-commonjs": "error",
+        "import/no-amd": "error",
+        "import/no-duplicates": "error",
+        "import/no-dynamic-require": "error",
+        "import/no-extraneous-dependencies": "error",
+        "import/no-mutable-exports": "error"
+    }
+};
